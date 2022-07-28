6be6201cc56be76740b2248b23eed089f6d98731
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2022-07-28 15:17:17 +0700

    tạo lỗi

--- src/main/java/com/example/demogit/GitKhanh.java
+++ src/main/java/com/example/demogit/GitKhanh.java
@@ -3,5 +3,6 @@ package com.example.demogit;
 public class GitKhanh {
     public static void main(String[] args) {
         System.out.println("HEllo Khánh");
+        System.out.println("HEllo CodeGym");
     }
 }
